69fd31124380768cdcff6fd52189d3a273b06f25
Juulia Santala <jusantal@cisco.com>
2025-01-15 21:12:26 +0200

    files for chapter 1 and 2

---
+++ lab_files/ch2/enable_ipv6.py
@@ -0,0 +1,55 @@
+from ncclient import manager
+
+def enable_ipv6(device_ip, username, password, port=830, verify=False)->None:
+
+    print(f"\nConnecting to device {device_ip}...", end=" ")
+
+    device = {
+        "host": device_ip,
+        "port": port,
+        "username": username,
+        "password": password,
+        "hostkey_verify": verify
+    }
+
+    payload = """
+        <config>
+            <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                <ipv6>
+                    <unicast-routing/>
+                    <router>
+                        <ospf>
+                            <id>1</id>
+                        </ospf>
+                    </router>
+                </ipv6>
+            </native>
+        </config>
+    """
+
+    try:
+        with manager.connect(**device, device_params={"name":"iosxe"},timeout=10) as connection:
+            print("success!")
+            response = connection.edit_config(target="running", config=payload)
+            print(response)
+
+    except Exception as err:
+        print("failed!")
+        print(err)
+        return 1
+
+if __name__ == "__main__":
+    devices = [
+        "198.18.133.101",
+        "198.18.7.2",
+        "198.18.11.2",
+        "198.18.12.2"
+    ]
+
+    credentials = {
+        "password": "C1sco12345",
+        "username": "developer"
+    }
+
+    for device in devices:
+        enable_ipv6(device, credentials["username"], credentials["password"])
