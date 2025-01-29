6f45710375a0a9b48f70540b65e1355ff3c878dc
Juulia Santala <jusantal@cisco.com>
2025-01-29 13:42:30 +0200

    setup init

---
+++ lab_setup.sh
@@ -0,0 +1,27 @@
+echo "Starting the lab setup script!"
+echo "********************"
+echo "\nPulling latest git files"
+
+cd /home/developer/lab-src
+git pull
+
+echo "********************"
+
+
+echo "\nInstalling requirements"
+
+cd /home/developer/lab-src
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "********************"
+
+echo "\nCreating lab directory"
+
+mkdir /home/developer/IPv6_lab
+
+cp -r /home/developer/lab-src/lab_files/ch* /home/developer/IPv6_lab
+
+echo "********************"
+
+echo "Lab setup script completed!"
